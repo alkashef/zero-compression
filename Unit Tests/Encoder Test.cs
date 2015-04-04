89b5f264954e71ab19c340212dd05fd60f58be91
alkashef <alkashef@users.noreply.github.com>
2015-04-04 23:24:26 +0200

    Initial commit

---
+++ Unit Tests/Encoder Test.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using ZeroCompression;
+
+namespace Unit_Tests
+{
+    [TestFixture]
+    class EncoderTest
+    {
+        // --------------------------------------------------------------------
+        
+        private Encoder _encoderUnderTest = new Encoder();
+
+        private List<int> _inputList = new List<int>();
+        private List<int> _refList = new List<int>();
+
+        // --------------------------------------------------------------------
+        
+        private bool CompareLists(List<int> list1, List<int> list2)
+        {
+            int firstNotSecond = list2.Except(list1).ToList().Count;
+            int secondNotFirst = list1.Except(list2).ToList().Count;
+            return ((firstNotSecond + secondNotFirst) == 0);
+        }
+
+        private bool EncodeAndCompare(List<int> inputList, List<int> refList)
+        {
+            List<int> outputList = _encoderUnderTest.Encode(inputList);
+            return CompareLists(outputList, refList);
+        }
+
+        // --------------------------------------------------------------------
+        
+        [Test]
+        public void ZerosInTheMiddle()
+        {
+            _inputList = new List<int>() { 0, 1, 0, 1, 0, 0, 0, 0, 0, 1 };
+            _refList = new List<int>() { 0, 1, 0, 1, 5, 1 };
+            Assert.AreEqual(true, EncodeAndCompare(_inputList, _refList));
+        }
+
+        [Test]
+        public void OnesInTheMiddle()
+        {
+            _inputList = new List<int>() { 1, 0, 1, 0, 1, 1, 1, 1, 1, 0 };
+            _refList = new List<int>() { 1, 0, 1, 0, 5 + 128, 0 };
+            Assert.AreEqual(true, EncodeAndCompare(_inputList, _refList));
+        }
+
+        [Test]
+        public void ZerosOnTheEdge()
+        {
+            _inputList = new List<int>() {0,0,0,0,1,0};
+            _refList = new List<int>() {4,1,0};
+            Assert.AreEqual(true, EncodeAndCompare(_inputList, _refList));
+        }
+
+        [Test]
+        public void OnesOnTheEdge()
+        {
+            _inputList = new List<int>() {0,1,0,1,1,1,1,1,1};
+            _refList = new List<int>() {0,1,0,6+128};
+            Assert.AreEqual(true, EncodeAndCompare(_inputList, _refList));
+        }
+
+        [Test]
+        public void AllZeros()
+        {
+            _inputList = new List<int>() {0,0,0,0,0,0,0,0};
+            _refList = new List<int>() {8};
+            Assert.AreEqual(true, EncodeAndCompare(_inputList, _refList));
+        }
+
+        [Test]
+        public void TwoSequencesConcatenated()
+        {
+            _inputList = new List<int>() {0,0,0,0,0,0,0,0,1,1,1};
+            _refList = new List<int>() {8,3+128};
+            Assert.AreEqual(true, EncodeAndCompare(_inputList, _refList));
+        }
+
+        [Test]
+        public void SequenceTooLong()
+        {
+            _inputList = new List<int>() {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1};
+            _refList = new List<int>() {10,5,1};
+            Assert.AreEqual(true, EncodeAndCompare(_inputList, _refList));
+        }
+    }
+}
---
+++ zeroCompression/Encoder.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace ZeroCompression
+{
+    public class Encoder
+    {
+        public List<int> Encode(List<int> inputList)
+        {
+            int counter = 1;
+            const int counterLimit = 10;
+
+            List<int> outputList = new List<int>();
+
+            for(int i = 1; i < inputList.Count; i++)
+            {
+                if (counter == counterLimit)
+                {
+                    compress(ref outputList, inputList[i - 1], ref counter);
+                    continue;
+                }
+                if (i == inputList.Count - 1) // last item
+                {
+                    if (inputList[i] == inputList[i - 1]) // last item is part of a sequence
+                    {
+                        counter++;
+                        compress(ref outputList, inputList[i - 1], ref counter);
+                    }
+                    else // last item is not part of a sequence
+                    {
+                        if (counter > 1) // a sequence just ended before the last item 
+                            compress(ref outputList, inputList[i - 1], ref counter); // add previous sequence
+                        else // item before last was not part of a sequence
+                            outputList.Add(inputList[i-1]); // add previous item 
+                        outputList.Add(inputList[i]); // add current item
+                    }
+                }
+                else // any item except the last
+                {
+                    if (inputList[i] == inputList[i - 1])
+                        counter++;
+                    else
+                    {
+                        if (counter <= 1) // not the end of a consequtive sequence of 1's or 0's
+                            outputList.Add(inputList[i - 1]);
+                        else
+                            compress(ref outputList, inputList[i - 1], ref counter);
+                    }
+                }
+            }
+            return outputList;
+        }
+
+        private void compress(ref List<int> list, int sequenceType, ref int sequenceLength)
+        {
+            if (sequenceType == 1) // compress 1's
+                list.Add(sequenceLength + 128);
+            else // compress 0's
+                list.Add(sequenceLength);
+
+            sequenceLength = 1; // reset sequence counter
+        }
+    }
+}
